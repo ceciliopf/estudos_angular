65a2d197a6091c773e24b018fceba028f4760f5d
Felipe Cecilio <felipepintocecilio@hotmail.com>
2026-08-07 09:52:23 -0300

    feat: created connection with db

---
+++ algamoney/src/main/java/com/algamoney_api/algamoney/AlgamoneyApplication.java
@@ -0,0 +1,13 @@
+package com.algamoney_api.algamoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgamoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgamoneyApplication.class, args);
+	}
+
+}
