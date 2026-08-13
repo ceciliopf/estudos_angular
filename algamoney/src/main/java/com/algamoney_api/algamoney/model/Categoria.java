3de7ae69a6773b5752aeec2f8416b5fa3306db92
Felipe Cecilio <felipepintocecilio@hotmail.com>
2026-08-13 11:14:59 -0300

    feat: tratei exceptions
    exceptions tratadas e mensagens personalizadas pelo messages.properties

--- algamoney/src/main/java/com/algamoney_api/algamoney/model/Categoria.java
+++ algamoney/src/main/java/com/algamoney_api/algamoney/model/Categoria.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "categoria")
@@ -16,6 +17,7 @@ public class Categoria {
     private Long codigo;
 
     @NotNull
+    @Size(min=3, max=20)
     private String nome;
 
     public Long getCodigo() {
