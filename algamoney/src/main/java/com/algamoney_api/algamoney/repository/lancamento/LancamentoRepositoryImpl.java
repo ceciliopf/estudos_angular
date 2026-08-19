08f281b731d8775ee5c1c64b0c2c4aefda81c186
Felipe Cecilio <felipepintocecilio@hotmail.com>
2026-08-19 15:00:55 -0300

    5.7 final

--- algamoney/src/main/java/com/algamoney_api/algamoney/repository/lancamento/LancamentoRepositoryImpl.java
+++ algamoney/src/main/java/com/algamoney_api/algamoney/repository/lancamento/LancamentoRepositoryImpl.java
@@ -2,14 +2,17 @@ package com.algamoney_api.algamoney.repository.lancamento;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Predicate;
+
+import org.springframework.util.ObjectUtils;
 
 import com.algamoney_api.algamoney.model.Lancamento;
+import com.algamoney_api.algamoney.model.Lancamento_;
 import com.algamoney_api.algamoney.repository.filter.LancamentoFilter;
 
 import io.micrometer.common.util.StringUtils;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.criteria.Predicate;
 import jakarta.persistence.TypedQuery;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
@@ -38,21 +41,22 @@ public class LancamentoRepositoryImpl implements LancamentoRepositoryQuery {
             Root<Lancamento> root) {
         List<Predicate> predicates = new ArrayList<>();
 
-        if (!StringUtils.isEmpty(lancamentoFilter.getDescricao())) {
+        if (!ObjectUtils.isEmpty(lancamentoFilter.getDescricao())) {
             predicates.add(builder.like(
-                    builder.lower(root.get("descricao")), "%" + lancamentoFilter.getDescricao().toLowerCase() + "% "));
+                    builder.lower(root.get(Lancamento_.descricao)), "%" + lancamentoFilter.getDescricao().toLowerCase() + "% "));
         }
 
-        if (!StringUtils.isEmpty(lancamentoFilter.getDataVencimentoDe())) {
+        if (!ObjectUtils.isEmpty(lancamentoFilter.getDataVencimentoDe())) {
+            predicates.add(
+                builder.greaterThanOrEqualTo(root.get(Lancamento_.dataVencimento), lancamentoFilter.getDataVencimentoDe()));
             // predicates.add(e);
         }
 
-        if (!StringUtils.isEmpty(lancamentoFilter.getDataVencimentoAte())) {
-            // predicates.add(e);
+        if (!ObjectUtils.isEmpty(lancamentoFilter.getDataVencimentoAte())) {
+        predicates.add(
+            builder.lessThanOrEqualTo(root.get(Lancamento_.dataVencimento), lancamentoFilter.getDataVencimentoAte()));
         }
 
-        return predicates.toArray(new Predicate[predicate.size()]);
-
-        return null;
+        return predicates.toArray(new Predicate[predicates.size()]);
     }
 }
