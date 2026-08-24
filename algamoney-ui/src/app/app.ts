9e63a62e38fcf48960531d68aa4c9b1ea3f36dd8
Felipe Cecilio <felipepintocecilio@hotmail.com>
2026-08-24 17:18:44 -0300

    9.3

--- algamoney-ui/src/app/app.ts
+++ algamoney-ui/src/app/app.ts
@@ -1,8 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BemVindo } from './bem-vindo/bem-vindo';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BemVindo],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
