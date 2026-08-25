061335da815bc781d72da2c64e40d66e7f041738
Felipe Cecilio <felipepintocecilio@hotmail.com>
2026-08-25 11:15:32 -0300

    9.7

--- algamoney-ui/src/app/app.ts
+++ algamoney-ui/src/app/app.ts
@@ -10,4 +10,17 @@ import { BemVindo } from './bem-vindo/bem-vindo';
 })
 export class App {
   nome = 'Thiago';
+
+  adicionar(){
+    console.log(`Adicionando ${this.nome}`);
+
+    const numero = Math.round(Math.random() * 100);
+    this.nome= "Joao " + numero;
+  }
+
+  alterarNome(event: any){
+    console.log(event);
+    this.nome = event.target.value
+  }
+
 }
