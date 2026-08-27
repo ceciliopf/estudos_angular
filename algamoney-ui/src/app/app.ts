5435114071e159a2b8130f12de2639d38bc53372
Felipe Cecilio <felipepintocecilio@hotmail.com>
2026-08-27 10:16:26 -0300

    9.12
    ngif e hidden

--- algamoney-ui/src/app/app.ts
+++ algamoney-ui/src/app/app.ts
@@ -2,18 +2,21 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BemVindo } from './bem-vindo/bem-vindo';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  imports: [RouterOutlet, BemVindo, FormsModule],
+  imports: [RouterOutlet, BemVindo, FormsModule, CommonModule],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
 })
 export class App {
   nome = 'Thiago';
+  adicionado = false;
 
   adicionar(){
     console.log(`Adicionando ${this.nome}`);
+    this.adicionado = true;
   }
 
 }
