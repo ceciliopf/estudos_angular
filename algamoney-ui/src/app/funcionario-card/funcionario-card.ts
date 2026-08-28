c54beb15f776c9a357ea5da3327b08990f8e4e50
Felipe Cecilio <felipepintocecilio@hotmail.com>
2026-08-28 09:20:56 -0300

    9.17

--- algamoney-ui/src/app/funcionario-card/funcionario-card.ts
+++ algamoney-ui/src/app/funcionario-card/funcionario-card.ts
@@ -1,7 +1,9 @@
 import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  imports: [],
+  imports: [FormsModule, CommonModule],
   selector: 'app-funcionario-card',
   // styleUrl: './funcionario-card.css',
   templateUrl: './funcionario-card.html',
@@ -14,4 +16,13 @@ import { Component, Input } from '@angular/core';
 })
 export class FuncionarioCard {
   @Input() funcionario: { id: number, nome: string } = { id: 0, nome: '' };
+
+  getEstilosCartao(){
+  return {
+
+     'border-width.px' : this.funcionario.id,
+     backgroundColor: this.funcionario.id % 2 === 0 
+     ? 'lightblue' : 'violet'
+  };
+}
 }
