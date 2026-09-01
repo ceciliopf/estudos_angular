232301765a0b62c4ea547098727d46981c3b315c
Felipe Cecilio <felipepintocecilio@hotmail.com>
2026-09-01 14:19:16 -0300

    10.3

--- algamoney-ui/src/app/app.ts
+++ algamoney-ui/src/app/app.ts
@@ -1,20 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
-import { FuncionarioCard } from './funcionario-card/funcionario-card';
-import { FuncionarioForm } from './funcionario-form/funcionario-form';
+import { TabViewModule } from 'primeng/tabview';
 
 @Component({
-  imports: [RouterOutlet, FormsModule, CommonModule, FuncionarioCard, FuncionarioForm],
+  imports: [RouterOutlet, TabViewModule],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
 })
 export class App {
-   funcionarios: { id: number, nome: string }[] = [];
-
-   aoAdicionar(funcionario: { id: number, nome: string }){
-    this.funcionarios.push(funcionario);
-}
+  protected readonly title = signal('algamoney-ui');
 }
