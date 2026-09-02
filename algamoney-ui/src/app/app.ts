ae15dea1aa956a6615addc4d1ceb3384bab6953a
Felipe Cecilio <felipepintocecilio@hotmail.com>
2026-09-02 17:33:04 -0300

    10.4

--- algamoney-ui/src/app/app.ts
+++ algamoney-ui/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TabViewModule } from 'primeng/tabview';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonDirective } from 'primeng/button';
 
 @Component({
-  imports: [RouterOutlet, TabViewModule],
+  imports: [RouterOutlet, TabViewModule, InputTextModule, ButtonDirective],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
