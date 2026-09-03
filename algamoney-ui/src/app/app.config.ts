bcf10d174d89797c253d61a0f9bdb77bb57975b8
Felipe Cecilio <felipepintocecilio@hotmail.com>
2026-09-03 09:09:45 -0300

    aula 10.5 criação de tabela com dados mockados

--- algamoney-ui/src/app/app.config.ts
+++ algamoney-ui/src/app/app.config.ts
@@ -1,9 +1,13 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
 import { routes } from './app.routes';
+import localePt from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localePt, 'pt-BR');
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,6 +18,7 @@ export const appConfig: ApplicationConfig = {
       theme: {
         preset: Aura
       }
-    })
+    }),
+    { provide: LOCALE_ID, useValue: 'pt-BR' }
   ]
 };
