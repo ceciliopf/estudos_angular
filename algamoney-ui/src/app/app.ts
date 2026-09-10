077399ddfe0b18d6e7a288f768bbfcab4fc6a26c
Felipe Cecilio <felipepintocecilio@hotmail.com>
2026-09-10 11:41:40 -0300

    10.11

--- algamoney-ui/src/app/app.ts
+++ algamoney-ui/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import {LancamentosPesquisa } from './lancamentos-pesquisa/lancamentos-pesquisa';
+import { Navbar } from './navbar/navbar';
 
 
 @Component({
-  imports: [ LancamentosPesquisa ],
+  imports: [ LancamentosPesquisa, Navbar],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
