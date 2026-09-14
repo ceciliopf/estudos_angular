76d23e7fbc86ec5ba3282f0eb8fe12efc36ffd07
Felipe Cecilio <felipepintocecilio@hotmail.com>
2026-09-14 14:14:43 -0300

    13.7

--- algamoney-ui/src/app/pessoas-cadastro/pessoas-cadastro.ts
+++ algamoney-ui/src/app/pessoas-cadastro/pessoas-cadastro.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
+import { InputMaskModule } from 'primeng/inputmask';
 
 @Component({
-  imports: [ FormsModule, InputTextModule, ButtonModule ],
+  imports: [FormsModule, InputTextModule, ButtonModule, InputMaskModule],
   selector: 'app-pessoas-cadastro',
   styleUrl: './pessoas-cadastro.css',
   templateUrl: './pessoas-cadastro.html',
