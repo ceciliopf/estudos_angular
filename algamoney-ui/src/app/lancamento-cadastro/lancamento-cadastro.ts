8bd70a057075fcee9dd63c42f477b108be221864
Felipe Cecilio <felipepintocecilio@hotmail.com>
2026-09-14 09:26:24 -0300

    13.3

--- algamoney-ui/src/app/lancamento-cadastro/lancamento-cadastro.ts
+++ algamoney-ui/src/app/lancamento-cadastro/lancamento-cadastro.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { TextareaModule } from 'primeng/textarea';
 import { CalendarModule } from 'primeng/calendar';
+import { SelectButtonModule } from 'primeng/selectbutton';
 
 @Component({
-  imports: [ InputTextModule, ButtonModule, TextareaModule, CalendarModule ],
+  imports: [ FormsModule, InputTextModule, ButtonModule, TextareaModule, CalendarModule, SelectButtonModule ],
   selector: 'app-lancamento-cadastro',
   styleUrl: './lancamento-cadastro.css',
   templateUrl: './lancamento-cadastro.html',
 })
-export class LancamentoCadastro {}
+export class LancamentoCadastro {
+  tipos = [
+    { label: 'Receita', value: 'RECEITA' },
+    { label: 'Despesa', value: 'DESPESA' }
+  ];
+}
+
