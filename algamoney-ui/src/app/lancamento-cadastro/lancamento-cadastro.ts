2302a684f407edf0981e53a45c978444b37a3766
Felipe Cecilio <felipepintocecilio@hotmail.com>
2026-09-14 10:21:25 -0300

    13.4

--- algamoney-ui/src/app/lancamento-cadastro/lancamento-cadastro.ts
+++ algamoney-ui/src/app/lancamento-cadastro/lancamento-cadastro.ts
@@ -5,9 +5,10 @@ import { ButtonModule } from 'primeng/button';
 import { TextareaModule } from 'primeng/textarea';
 import { CalendarModule } from 'primeng/calendar';
 import { SelectButtonModule } from 'primeng/selectbutton';
+import { DropdownModule } from 'primeng/dropdown';
 
 @Component({
-  imports: [ FormsModule, InputTextModule, ButtonModule, TextareaModule, CalendarModule, SelectButtonModule ],
+  imports: [FormsModule, InputTextModule, ButtonModule, TextareaModule, CalendarModule, SelectButtonModule, DropdownModule],
   selector: 'app-lancamento-cadastro',
   styleUrl: './lancamento-cadastro.css',
   templateUrl: './lancamento-cadastro.html',
@@ -17,5 +18,40 @@ export class LancamentoCadastro {
     { label: 'Receita', value: 'RECEITA' },
     { label: 'Despesa', value: 'DESPESA' }
   ];
-}
+  
+  categorias = [
+    {label: 'Alimentação', value: 'ALIMENTACAO'},
+    {label: 'Transporte', value: 'TRANSPORTE'},
+    {label: 'Moradia', value: 'MORADIA'},
+    {label: 'Saúde', value: 'SAUDE'},
+    {label: 'Lazer', value: 'LAZER'},
+    {label: 'Outros', value: 'OUTROS'}
+  ];
+  
+
+  pessoas = [
+    {label: 'João Silva', value: 'JOAO_SILVA'},
+    {label: 'Maria Silva', value: 'MARIA_SILVA'},
+    {label: 'Pedro Silva', value: 'PEDRO_SILVA'},
+    {label: 'Ana Silva', value: 'ANA_SILVA'},
+    {label: 'Carlos Silva', value: 'CARLOS_SILVA'},
+    {label: 'Marta Silva', value: 'MARTA_SILVA'},
+    {label: 'José Silva', value: 'JOSE_SILVA'},
+    {label: 'Francisca Silva', value: 'FRANCISCA_SILVA'},
+    {label: 'Antônio Silva', value: 'ANTONIO_SILVA'},
+    {label: 'Francisca Silva', value: 'FRANCISCA_SILVA'}
+  ];
+  
+
+  lancamento = {
+    tipo: 'RECEITA',
+    dataVencimento: new Date(),
+    dataPagamento: new Date(),
+    valor: 0,
+    descricao: '',
+    categoria: null,
+    pessoa: ''
+  }
+
+ }
 
