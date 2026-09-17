a172794ce28b19da23531d2c7041818e24a9149b
Felipe Cecilio <felipepintocecilio@hotmail.com>
2026-09-17 13:53:06 -0300

    17.11

--- algamoney-ui/src/app/app.ts
+++ algamoney-ui/src/app/app.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
-import {LancamentosPesquisa } from './lancamentos-pesquisa/lancamentos-pesquisa';
+import { LancamentosPesquisa } from './lancamentos-pesquisa/lancamentos-pesquisa';
 import { PessoasPesquisa } from './pessoas-pesquisa/pessoas-pesquisa';
 import { Navbar } from './navbar/navbar';
 import { LancamentoCadastro } from './lancamento-cadastro/lancamento-cadastro';
 import { PessoasCadastro } from './pessoas-cadastro/pessoas-cadastro';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
 
+registerLocaleData(localePt);
 
 @Component({
-  imports: [ LancamentosPesquisa, PessoasPesquisa, Navbar, LancamentoCadastro, PessoasCadastro ],
+  imports: [LancamentosPesquisa, PessoasPesquisa, Navbar, LancamentoCadastro, PessoasCadastro],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
+  providers: [],
 })
 export class App {
-
 }
