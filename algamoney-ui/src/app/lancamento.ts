52410331f8cbba90a54c33837dc121a90b021e71
Felipe Cecilio <137231839+ceciliopf@users.noreply.github.com>
2026-09-20 20:06:29 -0300

    18.6

--- algamoney-ui/src/app/lancamento.ts
+++ algamoney-ui/src/app/lancamento.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { firstValueFrom } from 'rxjs';
+import { Lancamento } from './core/model';
 
 
 export class LancamentoFiltro {
@@ -83,6 +84,82 @@ export class LancamentoService {
             .append('Authorization', `Bearer ${tokenResponse.access_token}`);
 
         return firstValueFrom(this.http.delete<void>(`${this.lancamentosUrl}/${codigo}`, { headers }));
+    }
+
+    async adicionar(lancamento: Lancamento): Promise<Lancamento> {
+        const tokenHeaders = new HttpHeaders()
+            .append('Content-Type', 'application/x-www-form-urlencoded')
+            .append('Authorization', 'Basic ' + btoa('angular:@ngul@r0'));
+
+        const body = new HttpParams()
+            .set('grant_type', 'client_credentials')
+            .set('scope', 'read write');
+
+        const tokenResponse: any = await firstValueFrom(
+            this.http.post(this.tokenUrl, body.toString(), { headers: tokenHeaders })
+        );
+
+        const headers = new HttpHeaders()
+            .append('Authorization', `Bearer ${tokenResponse.access_token}`)
+            .append('Content-Type', 'application/json');
+
+        return firstValueFrom(this.http.post<Lancamento>(this.lancamentosUrl, lancamento, { headers }));
+    }
+
+    async atualizar(lancamento: Lancamento): Promise<Lancamento> {
+        const tokenHeaders = new HttpHeaders()
+            .append('Content-Type', 'application/x-www-form-urlencoded')
+            .append('Authorization', 'Basic ' + btoa('angular:@ngul@r0'));
+
+        const body = new HttpParams()
+            .set('grant_type', 'client_credentials')
+            .set('scope', 'read write');
+
+        const tokenResponse: any = await firstValueFrom(
+            this.http.post(this.tokenUrl, body.toString(), { headers: tokenHeaders })
+        );
+
+        const headers = new HttpHeaders()
+            .append('Authorization', `Bearer ${tokenResponse.access_token}`)
+            .append('Content-Type', 'application/json');
+
+        return firstValueFrom(
+            this.http.put<Lancamento>(`${this.lancamentosUrl}/${lancamento.codigo}`, lancamento, { headers })
+        );
+    }
+
+    async buscarPorCodigo(codigo: number): Promise<Lancamento> {
+        const tokenHeaders = new HttpHeaders()
+            .append('Content-Type', 'application/x-www-form-urlencoded')
+            .append('Authorization', 'Basic ' + btoa('angular:@ngul@r0'));
+
+        const body = new HttpParams()
+            .set('grant_type', 'client_credentials')
+            .set('scope', 'read');
+
+        const tokenResponse: any = await firstValueFrom(
+            this.http.post(this.tokenUrl, body.toString(), { headers: tokenHeaders })
+        );
+
+        const headers = new HttpHeaders()
+            .append('Authorization', `Bearer ${tokenResponse.access_token}`);
+
+        return firstValueFrom(this.http.get<Lancamento>(`${this.lancamentosUrl}/${codigo}`, { headers }))
+            .then((lancamento: Lancamento) => {
+                this.converterStringsParaDatas([lancamento]);
+                return lancamento;
+            });
+    }
+
+    private converterStringsParaDatas(lancamentos: Lancamento[]) {
+        for (const lancamento of lancamentos) {
+            if (lancamento.dataVencimento) {
+                lancamento.dataVencimento = new Date(lancamento.dataVencimento + 'T00:00:00');
+            }
+            if (lancamento.dataPagamento) {
+                lancamento.dataPagamento = new Date(lancamento.dataPagamento + 'T00:00:00');
+            }
+        }
     }
 
     private formatDate(date: Date): string {
